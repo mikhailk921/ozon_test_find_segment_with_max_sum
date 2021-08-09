ca87c0eb1987dd018a0f566186897cb3fec42551
mikhailk921 <kozlovm62@yandex.ru>
2021-08-09 19:08:10 +0300

    Add array of all possible variants

--- src/find_subarray_with_max_sum.py
+++ src/find_subarray_with_max_sum.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
 
-def findSubarrayWithMaxSum(inputArray):
 
+def find_subarray_with_max_sum(input_array):
+    if not isinstance(input_array, list) or len(input_array) == 0:
+        exit()
 
+    result_list = list()
+
+    for i in range(len(input_array)-1):
+        max_sum = -sys.maxsize
+        indexes = tuple()
+        for j in range(i+1, len(input_array)):
+            local_sum = sum(input_array[i:j+1])
+            if local_sum > max_sum:
+                max_sum = local_sum
+                indexes = (i, j)
+            else:
+                break
+            print('{}; {}'.format(i, j))
+        print('max_sum = {}'.format(max_sum))
+        result_list.append({indexes: max_sum})
+    print('result = {}'.format(result_list))
     return tuple()
 
 
 
 if __name__ == '__main__':
-    findSubarrayWithMaxSum([10, -3, -12, 8, 42, 1, -7, 0, 3])
+    find_subarray_with_max_sum([10, -3, -12, 8, 42, 1, -7, 0, 3])
     # cases = [
     #     ([10, -3, -12, 8, 42, 1, -7, 0, 3], (3, 5)),
     #     #([1, 2, 3, 4, 5], (1, 2)),
