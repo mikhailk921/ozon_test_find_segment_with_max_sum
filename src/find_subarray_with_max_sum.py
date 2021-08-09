0a2befa623b127ee7235b9a9d4c187f96b2ab257
mikhailk921 <kozlovm62@yandex.ru>
2021-08-09 16:45:29 +0300

    Crate project structure

---
+++ src/find_subarray_with_max_sum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def findSubarrayWithMaxSum(inputArray):
+
+
+    return tuple()
+
+
+
+if __name__ == '__main__':
+    findSubarrayWithMaxSum([10, -3, -12, 8, 42, 1, -7, 0, 3])
+    # cases = [
+    #     ([10, -3, -12, 8, 42, 1, -7, 0, 3], (3, 5)),
+    #     #([1, 2, 3, 4, 5], (1, 2)),
+    #     #([-5, -4, -3, -2, -1], (1, 2)),
+    # ]
+    #
+    # for array, expected_result in cases:
+    #     assert findSubarrayWithMaxSum(array) == expected_result
